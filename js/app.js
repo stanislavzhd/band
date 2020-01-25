ad2deaf14eae6282911f2b2d14d1ce501eaafad9
Stanislav Zhdan <stanislav.zhdan@gmail.com>
2020-01-26 01:09:37 +0200

    partly added tours block

--- js/app.js
+++ js/app.js
@@ -9,3 +9,29 @@ document.body.onload = function () {
 };
 
 $('.gw-slider').slick();
+
+$('.gw-tours-slider').slick({
+    centerMode: true,
+    centerPadding: '60px',
+    slidesToShow: 3,
+    responsive: [
+        {
+            breakpoint: 768,
+            settings: {
+                arrows: false,
+                centerMode: true,
+                centerPadding: '40px',
+                slidesToShow: 3
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                arrows: false,
+                centerMode: true,
+                centerPadding: '40px',
+                slidesToShow: 1
+            }
+        }
+    ]
+});
